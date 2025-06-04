bc0d711eaa14c5f3041a0a19e2e2789cf71162fb
Ramon Saviato <ramonsaviato@hotmail.com>
2025-06-04 14:57:30 -0300

    fix: change all Instant fields underlying database types
    Having an java.time.Instant field mapped to a "TIMESTAMP WITHOUT TIME ZONE"
    field was seeding the database improperly. Essentially, the database was seeing
    a time stamp such as "2017-09-25T18:00:00Z" and stripping the "Z", since it is
    "WITHOUT TIME ZONE", converting it to a local time stamp and storing that.

--- src/main/java/com/devsuperior/desafio/domain/entities/domain/entities/Bloco.java
+++ src/main/java/com/devsuperior/desafio/domain/entities/domain/entities/Bloco.java
@@ -29,10 +29,10 @@ public class Bloco {
     @EqualsAndHashCode.Include
     private Long id;
 
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
     private Instant inicio;
 
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
     private Instant fim;
 
     @ManyToOne
